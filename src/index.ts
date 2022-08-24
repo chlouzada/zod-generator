a300725d6ebe832b66c1db47554f5db504ee00bb
carlos <chlouzada@gmail.com>
2022-08-23 22:26:33 -0300

    recursively

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,22 @@ const program = ts.createProgram([filename], {});
 const sourceFile = program.getSourceFile(filename)!;
 const typeChecker = program.getTypeChecker();
 
+const extract = (s: ts.Symbol): any => {
+  if ((s as any).type.intrinsicName)
+    return `${s.escapedName}:${(s as any).type.intrinsicName}`;
+
+  if ((s as any).type.members.size == 1) {
+    const key = Array.from((s as any).type.members.keys())[0];
+    return extract((s as any).type.members.get(key));
+  }
+
+  let aux: any[] = [];
+  for (const key of Array.from((s as any).type.members.keys())) {
+    aux.push(extract((s as any).type.members.get(key)));
+  }
+  return `${s.escapedName}:{${aux}}`;
+};
+
 function recursivelyPrintVariableDeclarations(
   node: ts.Node,
   sourceFile: ts.SourceFile
@@ -14,23 +30,15 @@ function recursivelyPrintVariableDeclarations(
     if (!nodeText.includes("resolver")) return;
 
     const type = typeChecker.getTypeAtLocation(node);
-    const typeName = typeChecker.typeToString(type, node);
-
-    console.log(nodeText);
-    console.log(`(${typeName})`);
 
     const returned = type
       .getCallSignatures()[0]
       .getReturnType()
       .getProperties();
 
-    const arr = returned.map(
-      (e) => `${e.escapedName}: z.${(e as any).type.intrinsicName}()`
-    );
+    const arr = returned.map((e) => extract(e));
 
-    console.log(`const schema = { ${arr.join(`, `)} }`);
-
-    // returned[0] as any).type.intrinsicName
+    console.log(`const schema = {${arr.join(`,`)}}`);
   }
 
   node
--- src/target.ts
+++ src/target.ts
@@ -1,21 +1,17 @@
-const a = 1
-
-const b = 'ok'
-
-
-function resolver(name: string) {
-  return {
-    age: 30,
-    name,
-  };
-}
 
 const resolver2 = (name: string) => {
-  const a = () => 30
+  const a = () => 30;
 
   return {
     age: a(),
     name,
-    test: `ok`
+    test: `ok`,
+
+    test2: {
+      agora: `nao`,
+      e:  a(),
+      name,
+      test: `ok`,
+    },
   };
-}
+};
